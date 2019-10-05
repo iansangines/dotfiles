13600b681a2b0597c4bfa3fe83fd8972f4bb3696
Ian <iansangines@gmail.com>
2019-10-05 11:12:49 +0200

    Modify setup-home-screens perms because of the startup

--- setup-home-screens.sh
+++ setup-home-screens.sh
@@ -7,7 +7,10 @@ xrandr --setprovideroutputsource 2 0
 
 # Displaying DVI output on the right of the Laptop screen
 xrandr --output DVI-I-2-1 --auto --right-of eDP-1
+
 # Displaying HDMI output on the right of the DVI screen
 xrandr --output DVI-I-3-2 --auto --right-of DVI-I-2-1
+
+#xrandr --output eDP-1 --off
 
 # --TODO: Dynamic depending of the output of the xrandr --listproviders and xrandr --current command
