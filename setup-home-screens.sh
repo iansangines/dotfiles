b3d1d431b219b7379fe9c81c59c0038eda7746df
Ian <iansangines@gmail.com>
2020-02-07 18:00:45 +0100

    Fixing setup-home-screens.sh

--- setup-home-screens.sh
+++ setup-home-screens.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# els noms dels dispositius i les pantalles surt de xrandr --current
 # Setting dock DVI provider
 xrandr --setprovideroutputsource 1 0
 # Setting dock HDMI provider
 xrandr --setprovideroutputsource 2 0
 
 # Displaying DVI output on the right of the Laptop screen
-xrandr --output DVI-I-2-1 --auto --right-of eDP-1
+xrandr --output DVI-I-2-1 --auto --primary
 
 # Displaying HDMI output on the right of the DVI screen
 xrandr --output DVI-I-3-2 --auto --right-of DVI-I-2-1
 
-#xrandr --output eDP-1 --off
+# Turning off laptop screen
+xrandr --output eDP-1 --off
 
 # --TODO: Dynamic depending of the output of the xrandr --listproviders and xrandr --current command
